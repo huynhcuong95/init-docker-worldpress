0b1cf7a7548193db1a172bf42b3ae203b6e0e4b7
cuonghuynh <cuonghuynh936@gmail.com>
2025-07-16 10:21:16 +0700

    fix

--- init.sql.sh
+++ init.sql.sh
@@ -10,7 +10,7 @@ echo "✅ MySQL is ready."
 
 echo "🔁 Replacing localhost URLs..."
 
-sed -E 's|http:\\/\\/localhost:8000\\/wordpress|http:\\/\\/thangmay.epms.vn|g; s|http:\\/\\/localhost:8000|http:\\/\\/thangmay.epms.vn|g' /home/worldtech/sources/worldpress/db/wordpress.sql > /tmp/updated.sql
+sed -E 's|http:\\/\\/localhost:8000\\/wordpress|http:\\/\\/thangmay.epms.vn|g; s|http:\\/\\/localhost:8000|http:\\/\\/thangmay.epms.vn|g' /db/wordpress.sql > /tmp/updated.sql
 
 echo "📥 Importing SQL into database..."
 mysql -h db -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /tmp/updated.sql
