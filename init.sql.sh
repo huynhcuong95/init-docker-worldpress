d19fd969656d65757c6d24aa192ccb61e2e94e26
cuonghuynh <cuonghuynh936@gmail.com>
2025-07-16 09:44:05 +0700

    fix

--- init.sql.sh
+++ init.sql.sh
@@ -10,7 +10,7 @@ echo "✅ MySQL is ready."
 
 echo "🔁 Replacing localhost with env URL..."
 
-sed -E "s|http://localhost:8000|${URL_WORDPRESS}|g; s|\blocalhost:8000\b|${URL_WORDPRESS}|g" /db/wordpress.sql > /tmp/updated.sql
+sed -E "s|http://localhost:8000|${URL_WORDPRESS}|g; s|\blocalhost:8000/wordpress\b|${URL_WORDPRESS}|g" /db/wordpress.sql > /tmp/updated.sql
 
 echo "📥 Importing SQL into database..."
 mysql -h db -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /tmp/updated.sql
