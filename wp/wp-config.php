d9f6e19f8eb1c7f7eec2f2b31858f0ac326c5bc4
cuonghuynh <cuonghuynh936@gmail.com>
2025-07-17 10:45:05 +0700

    fix

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -1,4 +1,5 @@
 <?php
+$_SERVER['HTTPS'] = 'on';
 /**
  * The base configuration for WordPress
  *
@@ -19,69 +20,88 @@
  *
  * @package WordPress
  */
+
 // IMPORTANT: this file needs to stay in-sync with https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
 // (it gets parsed by the upstream wizard in https://github.com/WordPress/WordPress/blob/f27cb65e1ef25d11b535695a660e7282b98eb742/wp-admin/setup-config.php#L356-L392)
+
 // a helper function to lookup "env_FILE", "env", then fallback
 if (!function_exists('getenv_docker')) {
-    // https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
-    function getenv_docker($env, $default) {
-        if ($fileEnv = getenv($env . '_FILE')) {
-            return rtrim(file_get_contents($fileEnv), "\r\n");
-        }
-        else if (($val = getenv($env)) !== false) {
-            return $val;
-        }
-        else {
-            return $default;
-        }
-    }
+	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
+	function getenv_docker($env, $default) {
+		if ($fileEnv = getenv($env . '_FILE')) {
+			return rtrim(file_get_contents($fileEnv), "\r\n");
+		}
+		else if (($val = getenv($env)) !== false) {
+			return $val;
+		}
+		else {
+			return $default;
+		}
+	}
 }
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
+
 /** Database username */
 define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
+
 /** Database password */
 define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
+
 /**
  * Docker image fallback values above are sourced from the official WordPress installation wizard:
  * https://github.com/WordPress/WordPress/blob/1356f6537220ffdc32b9dad2a6cdbe2d010b7a88/wp-admin/setup-config.php#L224-L238
  * (However, using "example username" and "example password" in your database is strongly discouraged.  Please use strong, random credentials!)
  */
+
 /** Database hostname */
 define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mysql') );
+
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8mb4') );
+define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
+
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
+
 /**#@+
  * Authentication unique keys and salts.
  *
  * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
  *
  * You can change these at any point in time to invalidate all existing cookies.
  * This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         getenv_docker('WORDPRESS_AUTH_KEY',         'put your unique phrase here') );
-define( 'SECURE_AUTH_KEY',  getenv_docker('WORDPRESS_SECURE_AUTH_KEY',  'put your unique phrase here') );
-define( 'LOGGED_IN_KEY',    getenv_docker('WORDPRESS_LOGGED_IN_KEY',    'put your unique phrase here') );
-define( 'NONCE_KEY',        getenv_docker('WORDPRESS_NONCE_KEY',        'put your unique phrase here') );
-define( 'AUTH_SALT',        getenv_docker('WORDPRESS_AUTH_SALT',        'put your unique phrase here') );
-define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', 'put your unique phrase here') );
-define( 'LOGGED_IN_SALT',   getenv_docker('WORDPRESS_LOGGED_IN_SALT',   'put your unique phrase here') );
-define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       'put your unique phrase here') );
+define( 'AUTH_KEY',         getenv_docker('WORDPRESS_AUTH_KEY',         '8da3b6e6d051278c1737049ad149a7c1fff4cbee') );
+define( 'SECURE_AUTH_KEY',  getenv_docker('WORDPRESS_SECURE_AUTH_KEY',  '606cda1cf4bf199a825ab6f779b02119da37b8ad') );
+define( 'LOGGED_IN_KEY',    getenv_docker('WORDPRESS_LOGGED_IN_KEY',    'ea32f7a5ee546855042f6f0e1a591a7495fc83d6') );
+define( 'NONCE_KEY',        getenv_docker('WORDPRESS_NONCE_KEY',        'c3ca849a55300f57c7f7dd630b657575767b8c50') );
+define( 'AUTH_SALT',        getenv_docker('WORDPRESS_AUTH_SALT',        '57651356720de0ca9eeb5e0e3d02001fd02565e6') );
+define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', '0692a70fd4ed8cf1270c47d8788e9fabaecd7226') );
+define( 'LOGGED_IN_SALT',   getenv_docker('WORDPRESS_LOGGED_IN_SALT',   'd2e5329bd85f7525071c4691e2c418e678f16162') );
+define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       '5c90f12e8e2fde0af0da5022a4e4f46494a7646b') );
 // (See also https://wordpress.stackexchange.com/a/152905/199287)
+
 /**#@-*/
+
 /**
  * WordPress database table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
 $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
+
 /**
  * For developers: WordPress debugging mode.
  *
@@ -95,39 +115,59 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
 define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
+
 /* Add any custom values between this line and the "stop editing" line. */
 
-// ===================================================================
-// === CẤU HÌNH ĐỂ GIẢI QUYẾT LỖI HTTPS CLOUDFLARE PROXY ===
-// ===================================================================
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also https://wordpress.org/support/article/administration-over-ssl/#using-a-reverse-proxy
+// if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+// 	$_SERVER['HTTPS'] = 'on';
+// }
+// (we include this by default because reverse proxying is extremely common in container environments)
 
 // Bước 1: Giúp WordPress nhận biết nó đang chạy qua HTTPS
-// Điều này là quan trọng nhất khi dùng Cloudflare proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
 }
 
-// Bước 2: Ép buộc WordPress sử dụng HTTPS
-// Cách này sẽ phá vỡ vòng lặp redirect
-define('WP_HOME', 'https://thangmay.epms.vn');
-define('WP_SITEURL', 'https://thangmay.epms.vn');
-
-// Bước 3: Vô hiệu hóa các chuyển hướng tự động của WordPress
-// Điều này ngăn WordPress tự tạo ra các chuyển hướng
+// Bước 2: Vô hiệu hóa hoàn toàn các tính năng chuyển hướng của WordPress
+// Điều này NGĂN CHẶN vòng lặp redirect ngay từ gốc
 define('FORCE_SSL_ADMIN', false);
 define('FORCE_SSL_LOGIN', false);
 
-// Bước 4: Cấu hình cho môi trường proxy (như Cloudflare)
+// Bước 3: ÉP BUỘC WordPress phải sử dụng URL HTTPS, bỏ qua giá trị trong database
+// Đây là phần quan trọng nhất để phá vỡ vòng lặp
+add_filter('option_home', function($old) {
+    return 'https://thangmay.epms.vn';
+});
+add_filter('option_siteurl', function($old) {
+    return 'https://thangmay.epms.vn';
+});
+
+// Bước 4: Cấu hình bảo mật và proxy
+define('WP_HOME', 'https://thangmay.epms.vn');
+define('WP_SITEURL', 'https://thangmay.epms.vn');
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+
+// Cấu hình cho môi trường proxy (như Cloudflare)
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
     $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
 }
+
+// ===================================================================
+// === KẾT THÚC CẤU HÌNH ===
+// ===================================================================
+
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', __DIR__ . '/' );
 }
+
+header('Access-Control-Allow-Origin: https://thangmay.epms.vn');
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
